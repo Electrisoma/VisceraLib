06e5f7aad984856879eb14e6e21f78946ffb69b2
Electrisoma <electrisoma@gmail.com>
2026-02-21 17:53:00 -0600

    archive -archived stuff for clarity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,12 +30,12 @@ fun module(name: String) {
 module("visceralib")                        // bundle of all modules into 1 project transitively, meant for publication
 //module("visceralib-configs-api-v1")         // config screen helper
 module("visceralib-core")                   // basic infrastructure for all modules
-module("visceralib-datagen-api-v1")         // platform-agnostic data providers
-module("visceralib-dsp-api-v1")             // event-driven digital sound processing effects
-module("visceralib-item-hooks-v1")          // platform-agnostic interface injection based item hooks
+//module("visceralib-datagen-api-v1")         // platform-agnostic data providers
+//module("visceralib-dsp-api-v1")             // event-driven digital sound processing effects
+//module("visceralib-item-hooks-v1")          // platform-agnostic interface injection based item hooks
 //module("visceralib-model-loading-api-v1")   // custom model loading helpers
 //module("visceralib-networking-api-v1")      // packet distribution and creation
-module("visceralib-registration-api-v1")    // deferred registration system
-module("visceralib-rendering-api-v1")       // rendering helpers and events
-module("visceralib-splashes-v1")            // custom resource pack and event-driven splash texts
+//module("visceralib-registration-api-v1")    // deferred registration system
+//module("visceralib-rendering-api-v1")       // rendering helpers and events
+//module("visceralib-splashes-v1")            // custom resource pack and event-driven splash texts
 //module("visceralib-ui-api-v1")              // ui modification and screen creation, might merge splashes into this
